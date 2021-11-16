047c3c435ff252d5f12665ad522c42c675e116a6
Iasemin-Florin <queue.triple@gmail.com>
2021-11-17 00:38:02 +0100

    added User class and User Tests

---
+++ Users.Tests/UserTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Users;
+using Xunit;
+
+namespace Users.Tests
+{
+    public class UserTests
+    {
+        [Fact]
+        public void UserShouldBeAdded()
+        {
+            // Expected
+            string expected = "user added Joe";
+
+
+            // Act
+            string actual = User.AddUser("kasper");
+
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void UserShouldBeDeleted()
+        {
+            // Expected
+            string expected = "user deleted Joe";
+
+            // Act
+            string actual = User.DeleteUser("kasper");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void UserShouldBeUpdated()
+        {
+            // Expected
+            string expected = "user name changed to Jonas";
+
+            // Act
+            string actual = User.UpdateUser("alexander", "Jonas");
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
---
+++ Users/Users.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Users
+{
+    public class User
+    {
+        public static List<string> people = new List<string>()
+        {
+            "Johan",
+            "Jasmin",
+            "Phillip",
+            "Alex"
+        };
+
+        public static string AddUser(string name)
+        {
+            people.Add(name);
+
+            if (people.Contains(name))
+            {
+                return $"user added {name}";
+            }
+
+            return null;
+        }
+
+        public static string DeleteUser(string name)
+        {
+            people.Remove(name);
+
+            if (!people.Contains(name))
+            {
+                return $"user deleted {name}";
+            }
+
+            return null;
+        }
+
+        public static string UpdateUser(string currentName, string newName)
+        {
+            if (!people.Contains(currentName))
+            {
+                return null;
+            }
+
+            people.Remove(currentName);
+
+            people.Add(newName);
+
+            if (people.Contains(newName))
+            {
+                return $"user name changed to {newName}";
+            }
+
+            return null;
+        }
+    }
+}
