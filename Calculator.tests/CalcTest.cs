50444f84ec5e231c13b2a4eebc8df5fba5ba9edc
Iasemin-Florin Dumitrescu <iasemin-florin@Iasemin-Florins-MacBook-Pro.local>
2021-11-16 08:22:58 +0100

    Project Set up

---
+++ Calculator.tests/CalcTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Xunit;
+namespace Calculator.tests
+{
+    public class CalcTest
+    {
+        [Fact]
+        public void AdditionSimpleValueShouldCalculate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Calculator/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Calculator
+{
+    public static class SimpCalc
+    {
+
+    }
+}
